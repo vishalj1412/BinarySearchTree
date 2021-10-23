ec5fadbb1a303129e51690f687dae3130a99db12
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-23 13:56:15 +0530

    Added: create tree as given

---
+++ MyBinaryNodeTest.java
@@ -0,0 +1,39 @@
+package com.bl.binarysearchtree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryNodeTest {
+
+    @Test
+    public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
+
+    @Test
+    public void givenNumbersWhenAddedToBSTShouldReturn13() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+
+        myBinaryTree.add(16);
+        myBinaryTree.add(3);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(13, size);
+    }
+}
